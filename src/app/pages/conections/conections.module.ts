33bc2eb1fef97c233844818eb65f0ac0f4b39e18
jeespoping <jeespoping@gmail.com>
2021-07-02 18:01:45 -0500

    terminado payments y connections

--- src/app/pages/conections/conections.module.ts
+++ src/app/pages/conections/conections.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ConectionsPageRoutingModule } from './conections-routing.module';
 
 import { ConectionsPage } from './conections.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ConectionsPageRoutingModule
+    ConectionsPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [ConectionsPage]
 })
